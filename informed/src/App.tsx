ace35558f2ba8eadcb3fce6b1f70613b9f0529ad
Aaron <chan.aaron73@gmail.com>
2022-01-15 18:01:37 -0800

    Initialized DeepAI.
    Co-authored-by: teaseann <teaseann@users.noreply.github.com>

--- informed/src/App.tsx
+++ informed/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Deep from './components/Deep';
 import './App.css';
 
 function App() {
-  return <h1>InforMED</h1>
+  return (<div>
+    <h1>InforMED</h1>
+    <Deep inputText={"Atrial fibrillation (A-fib) is an irregular and often very rapid heart rhythm (arrhythmia) that can lead to blood clots in the heart. A-fib increases the risk of stroke, heart failure and other heart-related complications. During atrial fibrillation, the heart's upper chambers (the atria) beat chaotically and irregularly out of sync with the lower chambers (the ventricles) of the heart. For many people, A-fib may have no symptoms. However, A-fib may cause a fast, pounding heartbeat (palpitations), shortness of breath or weakness. Episodes of atrial fibrillation may come and go, or they may be persistent. Although A-fib itself usually isn't life-threatening, it's a serious medical condition that requires proper treatment to prevent stroke. Treatment for atrial fibrillation may include medications, therapy to reset the heart rhythm and catheter procedures to block faulty heart signals. A person with atrial fibrillation may also have a related heart rhythm problem called atrial flutter. Although atrial flutter is a different arrhythmia, the treatment is quite similar to atrial fibrillation."}/> </div>)
 }
 
 export default App;
